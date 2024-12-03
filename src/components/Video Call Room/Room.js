ded09d005809b94770b5f3f05e3c26513fc4434e
uzafirahmad <uzafir525@gmail.com>
2024-12-03 14:53:44 +0500

    com

--- src/components/Video Call Room/Room.js
+++ src/components/Video Call Room/Room.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect, useContext } from 'react';
 import io from 'socket.io-client';
 import { useLocation, useSearchParams } from 'react-router-dom';
 import AppContext from '../AppContext';
+import Initial from './Initial'
+import Conversation from './Conversation'
 
 // Create socket connection outside the component to prevent multiple connections
 const socket = io('http://localhost:8080/vc', {
@@ -19,6 +21,23 @@ function Socket() {
     const [messageStatus, setMessageStatus] = useState(null);
     const [searchParams, setSearchParams] = useSearchParams();
     const room_id = searchParams.get("room_id")
+
+    const sendMessage = async (e) => {
+        e.preventDefault()
+
+        console.log(`${user.username}:`, e.target.user_message.value.trim())
+
+        if (room_id) {
+            socket.emit('send_message', {
+                room_id: room_id,
+                message: e.target.user_message.value.trim(),
+                username: user?.username,
+                accessToken: accessToken
+            }, (response) => {
+
+            });
+        }
+    }
 
     // Function to handle leaving the room
     const handleLeaveRoom = () => {
@@ -70,8 +89,8 @@ function Socket() {
             // console.log('Disconnected from WebSocket server');
         });
 
-        socket.on('message', (data) => {
-            console.log('Received message:', data);
+        socket.on('receive_message', (data) => {
+            console.log(`${data.username}:`, data.message);
         });
 
         socket.on('user_joined', (data) => {
@@ -99,12 +118,22 @@ function Socket() {
                 <div className="bg-green-100 p-3 rounded mt-4 flex justify-between items-center">
                     <p>Connected to Room:
                         <span className="ml-2 font-bold text-green-700">{room_id}</span>
+                        {" "}
+                        as
+                        {" "}
+                        <span className="ml-2 font-bold text-green-700">{user.username}</span>
                     </p>
                     <button
                         onClick={handleLeaveRoom}
                     >
                         Leave Room
                     </button>
+                    <form style={{ width: "200px", marginTop: "10px" }} onSubmit={sendMessage}>
+                        <input name='user_message' placeholder='Type Message' type='text' />
+                        <button type='submit'>
+                            Submit
+                        </button>
+                    </form>
                 </div>
             )}
         </div>
